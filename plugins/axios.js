4621c3e215e96388c6b61c2ab2f40527c3b14236
zhao chen <634605333@qq.com>
2021-07-08 21:01:15 +0800

    chore: clear some comment

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,23 +4,9 @@ export default function ({ $axios, redirect }) {
       redirect('/sorry')
     }
   })
-  //请求返回拦截，把数据返回到页面之前做些什么...
+  // 请求返回拦截，把数据返回到页面之前做些什么...
   $axios.interceptors.response.use((response) => {
-    // //特殊错误处理，状态为10时为登录超时
-    // if (response.data.code === 10) {
-    //   iView.Message.error("登录已超时，请重新登录");
-    //   router.push("/login")
-    // //其余错误状态处理    
-    // } else if (response.data.code != 0) {
-    //   iView.Message.error(response.data.msg)
-    // //请求成功
-    // } else if(response.data.code === 0){
-    //   //将我们请求到的信息返回页面中请求的逻辑
-    //   return response;
-    // }
-  //......
     return response?.data || response
-
   }, function (error) {
     return Promise.reject(error);
   });
